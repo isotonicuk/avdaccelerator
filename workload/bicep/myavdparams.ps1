a8b0fa33d229a3effea36daf413c735285846375
isotonicuk <73965805+isotonicuk@users.noreply.github.com>
2023-02-01 15:57:52 +0000

    main

---
+++ workload/bicep/myavdparams.ps1
@@ -0,0 +1,18 @@
+$avdVmLocalUserPassword = Read-Host -Prompt "Local user password" -AsSecureString
+New-AzSubscriptionDeployment `
+  -TemplateFile deploy-baseline.bicep `
+  -TemplateParameterFile deploy-baseline-parameters-example.json `
+  -avdWorkloadSubsId "2820ff07-405a-49c6-b74b-d78825165e02" `
+  -deploymentPrefix "AVD1" `
+  -avdVmLocalUserName "avdadmin" `
+  -avdVmLocalUserPassword $avdVmLocalUserPassword `
+  -avdIdentityServiceProvider "AAD" `
+  -existingHubVnetResourceId "<hubVnetResourceId>"  `
+  -customDnsIps "<customDNSservers>"  `
+  -avdEnterpriseAppObjectId "<wvdAppObjectId>" `
+  -avdVnetPrivateDnsZone $true `
+  -avdVnetPrivateDnsZoneFilesId "<PrivateDnsZoneFilesId>" `
+  -avdVnetPrivateDnsZoneKeyvaultId "<PrivateDnsZoneKeyvaultId>" `
+  -avdDeployMonitoring $true `
+  -deployAlaWorkspace $true `
+  -Location "eastus2"
